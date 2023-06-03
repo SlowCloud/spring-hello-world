da85951963a44e822cfd0216da6426730ba334ca
slowcloud <sju9417@gmail.com>
2023-06-03 16:50:18 +0900

    cors

---
+++ src/main/java/hello/world/demo/WebConfig.java
@@ -0,0 +1,17 @@
+package hello.world.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(HttpMethod.GET.name());
+    }
+}
